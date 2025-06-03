0339707317a045622db4984ae0a556ddfb272546
Jakub Jirutka <jakub@jirutka.cz>
2025-06-04 00:02:49 +0200

    Add credential_type to event in OTPFormAuthenticator and UsernamePasswordForm
    Closes #40118
    
    Signed-off-by: Jakub Jirutka <jakub@jirutka.cz>

--- services/src/main/java/org/keycloak/authentication/authenticators/browser/UsernamePasswordForm.java
+++ services/src/main/java/org/keycloak/authentication/authenticators/browser/UsernamePasswordForm.java
@@ -19,10 +19,12 @@ package org.keycloak.authentication.authenticators.browser;
 
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.Authenticator;
+import org.keycloak.events.Details;
 import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.models.credential.PasswordCredentialModel;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.services.managers.AuthenticationManager;
 
@@ -38,6 +40,8 @@ public class UsernamePasswordForm extends AbstractUsernameFormAuthenticator impl
 
     @Override
     public void action(AuthenticationFlowContext context) {
+        context.getEvent().detail(Details.CREDENTIAL_TYPE, PasswordCredentialModel.TYPE);
+
         MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
         if (formData.containsKey("cancel")) {
             context.cancelLogin();
